4f8f14435f4f71f062bc7f4183ced43c61744edf
Unknown <bsylve1996@gmail.com>
2018-05-23 20:24:43 -0500

    first real commit

---
+++ AnimooBot/Utilities.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+using System.IO;
+namespace AnimooBot
+{
+    class Utilities
+    {
+        private static Dictionary<string, string> alerts;
+
+        static Utilities()
+        {
+            string json = File.ReadAllText("SystemLang/alerts.json");
+            var data = JsonConvert.DeserializeObject<dynamic>(json);
+            alerts = data.ToObject<Dictionary<string, string>>();
+        }
+
+        public static string getAlert(string key)
+        {
+            if (alerts.ContainsKey(key))
+                return alerts[key];
+            else
+                return "";
+        }
+    }
+}
